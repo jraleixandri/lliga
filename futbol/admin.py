6adb475dd2af748fa3e22ba29503d04a48caaccb
Josep Ramon Aleixandri <jraleixandri@iesebre.com>
2025-05-29 17:44:15 +0200

    primer commit

--- futbol/admin.py
+++ futbol/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
+
 from .models import *
  
 admin.site.register(Lliga)
 admin.site.register(Equip)
 admin.site.register(Jugador)
-admin.site.register(Partit)
 
 class EventInline(admin.TabularInline):
     model = Event
